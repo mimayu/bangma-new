8a44f05f1f45e589bdbeca7cefc0dac5267eb616
alex <961227167@qq.com>
2018-12-30 18:00:29 +0800

    修改

--- src/views/remind/remind.vue
+++ src/views/remind/remind.vue
@@ -1,71 +1,41 @@
 <template>
     <div class="remind_container">
-        <vue-event-calendar :events="salelist" @monthChanged="" @dayChanged=""></vue-event-calendar>
+        <vue-event-calendar :events="saleLists" @day-changed="handleDay"></vue-event-calendar>
         <Tabs v-model="active">
             <Tab title="提醒" >
-                <ul>
-                    <li>
-                        <div>
-                            <i></i>
-                            <span>09：30</span>
-                        </div>
-                        <div></div>
-                        <div></div>
-                    </li>
-                </ul>
-                <dl class="remind-list">
-                    <dt class="on">10:55</dt>
-                    <dd class="orderNum">
-                        <a>
-                            <span>订单号:</span>
-                        </a>
-                        <span class="timeout">上门倒计时01：53</span>
-                        <span class="red">签约成功</span>
-                    </dd>
-                    <dd>
-                        <span>姓名:</span>
-                        巴啦啦
-                    </dd>
-                    <dd>
-                        <span>手机号:</span>
-                        18217157168
-                    </dd>
-                    <dd>
-                        <span>地址:</span>
-                        静安区 皇家大道 15号
-                    </dd>
-                </dl>
+                <div v-if="remindLists.length > 0">
+                    <dl class="remind-list" v-for="(item, index) in remindLists" :key="index">
+                        <dt class="on">10:55</dt>
+                        <dd class="orderNum">
+                            <a>
+                                <span>订单号:</span>
+                            </a>
+                            <span class="timeout">上门倒计时01：53</span>
+                            <span class="red">签约成功</span>
+                        </dd>
+                        <dd>
+                            <span>姓名:</span>
+                            巴啦啦
+                        </dd>
+                        <dd>
+                            <span>手机号:</span>
+                            18217157168
+                        </dd>
+                        <dd>
+                            <span>地址:</span>
+                            静安区 皇家大道 15号
+                        </dd>
+                    </dl>
+                </div>
+                <div v-else>
+                    暂无提醒
+                </div>
             </Tab>
 
             <Tab title="事项">
-                 <dl class="remind-list">
-                    <dt class="on">10:55</dt>
-                    <dd class="orderNum">
-                        <a>
-                            <span>订单号:</span>
-                        </a>
-                        <span class="timeout">上门倒计时01：53</span>
-                        <span class="red">签约成功</span>
-                    </dd>
-                    <dd>
-                        <span>姓名:</span>
-                        巴啦啦
-                    </dd>
-                    <dd>
-                        <span>手机号:</span>
-                        18217157168
-                    </dd>
-                    <dd>
-                        <span>地址:</span>
-                        静安区 皇家大道 15号
-                    </dd>
-                </dl>
-                <dl class="company">
-                    <dt>公司公告</dt>
-                    <dd>
-                        巴拉巴拉巴拉
-                    </dd>
-                </dl>
+                <div>
+                    暂无事项
+                </div>
             </Tab>
         </Tabs>
         
@@ -87,7 +57,8 @@
         },
         data () {
             return {
-                salelist: [],
+                saleLists: [],
+                remindLists: [],
                 active:0,
             }
         },
@@ -107,34 +78,16 @@
                 }
                 getCalendar(params).then(
                     res => {
-                        this.salelist = [{
-                            date: '2018/12/16', // Required
-                            time: '13:51:35',
-                            title: '订单',
-                            department_name: "总经办",
-                            'group_name': null,
-                            'iId': "38",
-                            'role_name': "业务员",
-                            'sMobilePhone': "13524761932",
-                            'sName': "刘厚贤",
-                            'timeCreated': "2018-11-16 13:51:35",
-                        },
-                        {
-                            date: '2018/11/16', // Required
-                            time: '13:51:35',
-                            title: '订单',
-                            department_name: "总经办",
-                            'group_name': null,
-                            'iId': "38",
-                            'role_name': "业务员",
-                            'sMobilePhone': "13524761932",
-                            'sName': "刘厚贤",
-                            'timeCreated': "2018-11-16 13:51:35",
-                        }];
-                        console.log('res', this.salelist);
+                        this.saleLists = res.allForDateArr;
+                        this.remindLists = res.allForDateArr;
+                        console.log('res', this.saleLists);
                     }
                 )
                 console.log('1');
+            },
+            handleDay(value) {
+                console.log('value', value);
+                this.remindLists = value.events;
             }
         }
     }
