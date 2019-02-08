7d77f7357e59125f44fa6c18f71f1c1a4ac0d6eb
luweiwei88 <luweiwei88@126.com>
2019-02-08 23:34:51 +0800

    开工的列表

---
+++ src/views/sale/saleWorking-org.vue
@@ -0,0 +1,207 @@
+<template>
+    <div class="saleWorking_container">
+        <List
+          v-model="loading"
+          :finished="finished"
+          finished-text="没有更多了"
+          @load="handleLoad"
+        >
+            <Cell-group class="group" v-for="item in customerLists" :key="item.iCustomerId">
+                <Cell title="订单号">
+                    <template>
+                        <div class="custom_wrap">
+                            <span class="order_id">{{item.iCustomerId}}</span>
+                            <span class="status">开工实施</span>
+                        </div>
+                    </template>
+                </Cell>
+                <Cell title="姓名" :value="item.sUsername" />
+                <Cell title="手机号" :value="item.sMobile" />
+                <Cell title="地址" :value="item.sAddress" />
+                <Cell title="施工内容" :value="item.sRemarks || '-'" />
+                <Cell title="预约时间" :value="item.tOrderDate || '-'" />
+                <div class="van-cell btn_wrap" v-if="item.actions">
+                    <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
+                </div>
+            </Cell-group>
+        </List>
+        <Actionsheet
+            v-model="modeShow"
+            :actions="actions"
+            @select="handleSelect"
+        />
+        <footerNav></footerNav>
+    </div>
+</template>
+
+<script>
+    import { Cell, CellGroup, Button, Field, Toast, Popup, Picker, List, Actionsheet } from 'vant';
+    import footerNav from "@/components/footerNav"; // 引入页脚    
+    import { getCustomer } from '@/server';
+
+    export default {
+        name: 'saleWorking',
+        components: {
+            Cell,
+            CellGroup,
+            Button,
+            Field,
+            Toast,
+            Popup, 
+            Picker,
+            List,
+            Actionsheet,
+            footerNav
+        },
+        data() {
+            return {
+                page: 1,
+                customerLists: [],
+                loading: false, // 是否加载
+                finished: false, // 是否结束
+                modeShow: false, // 报价显示
+                currentId: '', // 选择id
+                actions: [
+                    {
+                        name: '普装'
+                    },
+                    {
+                        name: '精装'
+                    },
+                    {
+                        name: '奢华'
+                    }
+                ]
+            };
+        },
+        methods: {
+            /*
+            * 点击按钮
+            * 3 -> 报价处理
+            * 5 -> 合同解除
+            * 8 -> 完工
+            */
+            handleClick(type, id) {
+                switch(type) {
+                    case 3:
+                        this.handleQuote(id);
+                        break;
+                    case 5:
+                        this.handleGo(id, type);
+                        break;
+                    case 8:
+                        this.handleGo(id, type);
+                        break;
+                    default:
+                        break;
+                }
+            },
+            /*
+            * 处理报价
+            */
+            handleQuote(id) {
+                this.modeShow = true;
+                this.currentId = id;
+            },
+            /*
+            * 处理报价选择
+            */
+            handleSelect(value) {
+                let mode = this.actions.findIndex(item => {
+                    return item.name == value.name
+                })
+                this.modeShow = false;
+                this.$router.push(
+                    {
+                        name: 'quotation',
+                        params: {
+                            id: this.currentId,
+                            mode: +mode + 1
+                        }
+                    }
+                )
+            },
+            /*
+            * 取消报价/完工
+            */
+            handleGo(id, type) {
+                let name = type == 5 ? 'quotationCancel' : 'bossFinishAdd';
+                this.$router.push(
+                    {
+                        name: name,
+                        params: {
+                            id: id
+                        }
+                    }
+                )
+            },
+            /*
+            * 处理加载
+            */
+            handleLoad() {
+                this.getCustomer();
+            },
+            /*
+            * 请求数据
+            */
+            getCustomer() {
+                let params = {
+                    status: 7,
+                    page: this.page
+                }
+                getCustomer(params).then(
+                    res => {
+                        if(res.success == 1) {
+                            this.customerLists = this.customerLists.concat(res.list);
+                            this.page += 1;
+                            this.loading = false;
+                            if(res.list.length == 0) {
+                                this.finished = true;
+                            }
+                        }
+                    }
+                )
+            },
+        }
+    }
+</script>
+
+<style lang="scss">
+    .saleWorking_container {
+        background-color: #f6f6f6;
+        .van-cell__title, .van-field .van-cell__title {
+            max-width: 100px;
+        }
+        .van-cell__title, .van-cell__value{
+            text-align: left;
+        }
+        .btn_wrap {
+            justify-content: flex-end;
+            button {
+                margin: 4px;
+            }
+        }
+        .assign_btn {
+            padding: 6px 8px;
+            margin-left: 9px;
+            border: 1px solid #ebedf0;
+            border-radius: 10px;
+            outline: none;
+            color: #333;
+            background: #fff;
+        }
+        .custom_wrap {
+            display: flex;
+        }
+        .order_id {
+            flex: 1;
+        }
+        .status {
+            width: 60px;
+            text-align: center;
+        }
+        .group {
+            margin-bottom: 8px;
+        }
+    }
+</style>
--- src/views/sale/saleWorking.vue
+++ src/views/sale/saleWorking.vue
@@ -1,169 +1,336 @@
 <template>
-    <div class="saleWorking_container">
+  <div class="saleWorking_container">
+    <Tabs v-model="active">
+      <Tab title="派工完成">
         <List
-          v-model="loading"
-          :finished="finished"
-          finished-text="没有更多了"
-          @load="handleLoad"
+          v-model="loading_paigong"
+          :finished="finished_paigong"
+          :finished-text="finished_paigong_text"
+          @load="handlePaigongLoad"
         >
-            <Cell-group class="group" v-for="item in customerLists" :key="item.iCustomerId">
-                <Cell title="订单号">
-                    <template>
-                        <div class="custom_wrap">
-                            <span class="order_id">{{item.iCustomerId}}</span>
-                            <span class="status">开工实施</span>
-                        </div>
-                    </template>
-                </Cell>
-                <Cell title="姓名" :value="item.sUsername" />
-                <Cell title="手机号" :value="item.sMobile" />
-                <Cell title="地址" :value="item.sAddress" />
-                <Cell title="施工内容" :value="item.sRemarks || '-'" />
-                <Cell title="预约时间" :value="item.tOrderDate || '-'" />
-                <div class="van-cell btn_wrap" v-if="item.actions">
-                    <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
-                </div>
-            </Cell-group>
+          <Cell-group class="group" v-for="item in data_paigong" :key="item.iCustomerId">
+            <Cell title="订单号">
+                <template>
+                    <div class="custom_wrap">
+                        <span class="order_id">{{item.iCustomerId}}</span>
+                        <span class="status">{{item.iStatus_name}}</span>
+                    </div>
+                </template>
+            </Cell>
+            <Cell title="姓名" :value="item.sUsername" />
+            <Cell title="手机号" :value="item.sMobile" />
+            <Cell title="地址" :value="item.sAddress" />
+            <Cell title="施工内容" :value="item.sRemarks || '-'" />
+            <Cell title="接单日期" :value="item.tOrderDate || '-'" />
+            <div class="van-cell btn_wrap" v-if="item.actions">
+              <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
+            </div>
+          </Cell-group>
         </List>
-        <Actionsheet
-            v-model="modeShow"
-            :actions="actions"
-            @select="handleSelect"
-        />
-        <footerNav></footerNav>
-    </div>
+      </Tab>
+      <Tab title="开工进场">
+        <List
+          v-model="loading_kaigong"
+          :finished="finished_kaigong"
+          :finished-text="finished_kaigong_text"
+          @load="handleKaigongLoad"
+        >
+          <Cell-group class="group" v-for="item in data_kaigong" :key="item.iCustomerId">
+            <Cell title="订单号">
+                <template>
+                    <div class="custom_wrap">
+                        <span class="order_id">{{item.iCustomerId}}</span>
+                        <span class="status">{{item.iStatus_name}}</span>
+                    </div>
+                </template>
+            </Cell>
+            <Cell title="姓名" :value="item.sUsername" />
+            <Cell title="手机号" :value="item.sMobile" />
+            <Cell title="地址" :value="item.sAddress" />
+            <Cell title="施工内容" :value="item.sRemarks || '-'" />
+            <Cell title="开工日期" :value="item.dateKaigong || '-'" />
+            <div class="van-cell btn_wrap" v-if="item.actions">
+              <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
+            </div>
+          </Cell-group>
+        </List>
+      </Tab>
+      <Tab title="完工验收">
+        <List
+          v-model="loading_yanshou"
+          :finished="finished_yanshou"
+          :finished-text="finished_yanshou_text"
+          @load="handleYanshouLoad"
+        >
+          <Cell-group class="group" v-for="item in data_yanshou" :key="item.iCustomerId">
+            <Cell title="订单号">
+                <template>
+                    <div class="custom_wrap">
+                        <span class="order_id">{{item.iCustomerId}}</span>
+                        <span class="status">{{item.iStatus_name}}</span>
+                    </div>
+                </template>
+            </Cell>
+            <Cell title="姓名" :value="item.sUsername" />
+            <Cell title="手机号" :value="item.sMobile" />
+            <Cell title="地址" :value="item.sAddress" />
+            <Cell title="施工内容" :value="item.sRemarks || '-'" />
+            <Cell title="完工日期" :value="item.dateWangong || '-'" />
+            <div class="van-cell btn_wrap" v-if="item.actions">
+              <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
+            </div>
+          </Cell-group>
+        </List>
+      </Tab>
+
+      <Tab title="完工收款">
+        <List
+          v-model="loading_wancheng"
+          :finished="finished_wancheng"
+          :finished-text="finished_wancheng_text"
+          @load="handleWanchengLoad"
+        >
+          <Cell-group class="group" v-for="item in data_wancheng" :key="item.iCustomerId">
+            <Cell title="订单号">
+                <template>
+                    <div class="custom_wrap">
+                        <span class="order_id">{{item.iCustomerId}}</span>
+                        <span class="status">{{item.iStatus_name}}</span>
+                    </div>
+                </template>
+            </Cell>
+            <Cell title="姓名" :value="item.sUsername" />
+            <Cell title="手机号" :value="item.sMobile" />
+            <Cell title="地址" :value="item.sAddress" />
+            <Cell title="施工内容" :value="item.sRemarks || '-'" />
+            <Cell title="收款日期" :value="item.dateWeikuan || '-'" />
+            <div class="van-cell btn_wrap" v-if="item.actions">
+              <button plain type="primary" class="assign_btn" v-for="(action, index) in item.actions" :key="action.type" @click="handleClick(action.type, item.iCustomerId)">{{action.name}}</button>
+            </div>
+          </Cell-group>
+        </List>
+      </Tab>
+
+
+    </Tabs>
+    <Actionsheet
+      v-model="modeShow"
+      :actions="actions"
+      @select="handleSelect"
+    />
+    <footerNav></footerNav>
+  </div>
 </template>
 
 <script>
-    import { Cell, CellGroup, Button, Field, Toast, Popup, Picker, List, Actionsheet } from 'vant';
-    import footerNav from "@/components/footerNav"; // 引入页脚    
-    import { getCustomer } from '@/server';
+  import { Tab, Tabs, Cell, CellGroup, Toast, List, Actionsheet } from 'vant';
+  import { getCustomer } from '@/server';
+  import footerNav from '../../components/footerNav' // 引入login.vue组件
 
-    export default {
-        name: 'saleWorking',
-        components: {
-            Cell,
-            CellGroup,
-            Button,
-            Field,
-            Toast,
-            Popup, 
-            Picker,
-            List,
-            Actionsheet,
-            footerNav
-        },
-        data() {
-            return {
-                page: 1,
-                customerLists: [],
-                loading: false, // 是否加载
-                finished: false, // 是否结束
-                modeShow: false, // 报价显示
-                currentId: '', // 选择id
-                actions: [
-                    {
-                        name: '普装'
-                    },
-                    {
-                        name: '精装'
-                    },
-                    {
-                        name: '奢华'
-                    }
-                ]
-            };
-        },
-        methods: {
-            /*
-            * 点击按钮
-            * 3 -> 报价处理
-            * 5 -> 合同解除
-            * 8 -> 完工
-            */
-            handleClick(type, id) {
-                switch(type) {
-                    case 3:
-                        this.handleQuote(id);
-                        break;
-                    case 5:
-                        this.handleGo(id, type);
-                        break;
-                    case 8:
-                        this.handleGo(id, type);
-                        break;
-                    default:
-                        break;
-                }
-            },
-            /*
-            * 处理报价
-            */
-            handleQuote(id) {
-                this.modeShow = true;
-                this.currentId = id;
-            },
-            /*
-            * 处理报价选择
-            */
-            handleSelect(value) {
-                let mode = this.actions.findIndex(item => {
-                    return item.name == value.name
-                })
-                this.modeShow = false;
-                this.$router.push(
-                    {
-                        name: 'quotation',
-                        params: {
-                            id: this.currentId,
-                            mode: +mode + 1
-                        }
-                    }
-                )
-            },
-            /*
-            * 取消报价/完工
-            */
-            handleGo(id, type) {
-                let name = type == 5 ? 'quotationCancel' : 'bossFinishAdd';
-                this.$router.push(
-                    {
-                        name: name,
-                        params: {
-                            id: id
-                        }
-                    }
-                )
-            },
-            /*
-            * 处理加载
-            */
-            handleLoad() {
-                this.getCustomer();
-            },
-            /*
-            * 请求数据
-            */
-            getCustomer() {
-                let params = {
-                    status: 7,
-                    page: this.page
-                }
-                getCustomer(params).then(
-                    res => {
-                        if(res.success == 1) {
-                            this.customerLists = this.customerLists.concat(res.list);
-                            this.page += 1;
-                            this.loading = false;
-                            if(res.list.length == 0) {
-                                this.finished = true;
-                            }
-                        }
-                    }
-                )
-            },
+  export default {
+    name: 'signStatus',
+    data () {
+      return {
+        active: 0,
+        data_paigong: [],
+        data_kaigong: [],
+        data_yanshou: [],
+        data_wancheng: [],
+        modeShow: false, // 选择模式
+        actions: [
+          {
+            name: '普装'
+          },
+          {
+            name: '精装'
+          },
+          {
+            name: '奢华'
+          }
+        ],
+        currentId: '', // 选择的id
+        loading_paigong: false, // 签约等待
+        finished_paigong: false, // 签约等待
+        finished_paigong_text: '没有更多了',
+        page_paigong: 1, // 签约等待
+        loading_kaigong: false, // 签约成功 
+        finished_kaigong: false, // 签约成功 
+        finished_kaigong_text: '没有更多了', // 签约成功 
+        page_kaigong: 1, // 签约成功 
+        loading_yanshou: false, // 签约失败 
+        finished_yanshou: false, // 签约失败 
+        finished_yanshou_text: '没有更多了', // 签约成功 
+        page_yanshou: 1, // 签约失败 
+        loading_wancheng: false, // 合同取消 
+        finished_wancheng: false, // 合同取消 
+        finished_wancheng_text: '没有更多了', // 合同取消 
+        page_wancheng: 1, // 合同取消 
+      }
+    },
+    components: {
+      Tab,
+      Tabs,
+      Cell, 
+      CellGroup,
+      Toast,
+      List,
+      Actionsheet,
+      'footerNav': footerNav,
+    },
+    created() {
+      this.active = this.$route.query.active || 0;
+    },
+    methods: {
+      /*
+      * 点击按钮
+      * 2 -> 上门
+      * 3 -> 报价处理
+      * 4 -> 报价调整
+      * 5 -> 合同解除
+      * 6 -> 派工
+      */
+      handleClick(type, id) {
+        switch(type) {
+          case 2:
+            this.handleGo(id, type);
+            break;
+          case 3:
+            this.handleQuote(id);
+            break;
+          case 4:
+            this.handleGo(id, type);
+            break;
+          case 5:
+            this.handleGo(id, type);
+            break;
+          case 6:
+            this.handleGo(id, type);
+            break;
+          default:
+            break;
         }
+      },
+      /*
+      * 处理上门/调整报价/取消报价/派工
+      */
+      handleGo(id, type) {
+        let name = '';
+        if(type == 2) {
+          name = 'visit' 
+        }
+        if(type == 4) {
+          name = 'quotationList' 
+        }
+        if(type == 5) {
+          name = 'quotationCancel' 
+        }
+        if(type == 6) {
+          name = 'managerPaigong' 
+        }
+        this.$router.push(
+          {
+              name: name,
+              params: {
+                  id: id
+              }
+          }
+        )
+      },
+      /*
+      * 处理报价
+      */
+      handleQuote(id) {
+        this.modeShow = true;
+        this.currentId = id;
+      },
+      /*
+      * 处理报价选择
+      */
+      handleSelect(value) {
+        let mode = this.actions.findIndex(item => {
+          return item.name == value.name
+        })
+        this.modeShow = false;
+        this.$router.push(
+          {
+              name: 'quotation',
+              params: {
+                  id: this.currentId,
+                  mode: +mode + 1
+              }
+          }
+        )
+      },
+      /*
+      * 加载派工完成数据
+      */
+      handlePaigongLoad() {
+        let params = {
+          status: 6,
+          page: this.page_paigong,
+        }
+        this.getInfo(params, 'paigong');
+      },
+      /*
+      * 加载开工进场数据
+      */
+      handleKaigongLoad() {
+        let params = {
+          status: 7,
+          page: this.page_kaigong
+        }
+        this.getInfo(params, 'kaigong');
+      },
+      /*
+      * 加载完工验收数据
+      */
+      handleYanshouLoad() {
+        let params = {
+          status: 8,
+          page: this.page_yanshou,
+        }
+        this.getInfo(params, 'yanshou');
+      },
+      /*
+      * 加载合同取消数据
+      */
+      handleWanchengLoad() {
+        let params = {
+          status: 9,
+          page: this.page_wancheng,
+        }
+        this.getInfo(params, 'wancheng');
+      },
+      /*
+      * 获取数据
+      */
+      getInfo(params, type) {
+        let dataType = `data_${type}`;
+        let finishedType = `finished_${type}`;
+        let loadingType = `loading_${type}`;
+        let pageType = `page_${type}`;
+        let finishedTetx = `finished_${type}_text`;
+
+        getCustomer(params).then(
+          res => {
+            if(res.success == 1) {
+              this[dataType] = this[dataType].concat(res.list);
+              this[loadingType] = false;
+              if(res.list.length == 0) {
+                this[finishedType] = true;
+              }
+              this[pageType] += 1;
+            }else {
+              this[finishedTetx] = res.msg;
+              this[loadingType] = false;
+              this[finishedType] = true;
+              Toast(res.msg);
+            }
+          }
+        )
+      }
     }
+  }
 </script>
 
 <style lang="scss">
