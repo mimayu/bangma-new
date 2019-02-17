b723a3477cd02fa942ed6faab9a6fb379330a72e
alex <961227167@qq.com>
2019-02-17 21:26:38 +0800

    修改

--- src/views/boss/bossHome.vue
+++ src/views/boss/bossHome.vue
@@ -57,58 +57,55 @@
 
     <Tabs v-model="active">
         <Tab title="今天">
-          <h3>数据统计</h3>
-
-          <Row class=mini>
-            <Col span="6">%</Col>
-            <Col span="6">上门率66%</Col>
-            <Col span="6">签单率66%</Col>
-            <Col span="6">%</Col>
-
-          </Row>
-          <Row class="order">
-            <Col span="6">30<br/>总接单数</Col>
-            <Col span="6">30<br/>总上门数</Col>
-            <Col span="6">30<br/>总签单数</Col>
-            <Col span="6">30<br/>总签单金额</Col>
-          </Row>
-          <h3>签单占比</h3>
-
-           
+            <h3>数据统计</h3>
+            <Row class=mini>
+                <Col span="6">%</Col>
+                <Col span="6">上门率{{data.shangmen_rate * 100}}%</Col>
+                <Col span="6">签单率{{data.qiandan_rate * 100}}%</Col>
+                <Col span="6">%</Col>
+            </Row>
+            <Row class="order">
+                <Col span="6">{{data.all_num}}<br/>总接单数</Col>
+                <Col span="6">{{data.shangmen_num}}<br/>总上门数</Col>
+                <Col span="6">{{data.qiandan_num}}<br/>总签单数</Col>
+                <Col span="6">{{data.qiandan_fee}}<br/>总签单金额</Col>
+            </Row>
+            <div class="chart-wrapper">
+                <h3>签单占比</h3>
+                <canvas id="visit" class="piechart"></canvas>
+            </div>
        </Tab>
-      <Tab title="本周">
-         <h3>数据统计</h3>
-
-          <Row class=mini>
-            <Col span="6">%</Col>
-            <Col span="6">上门率66%</Col>
-            <Col span="6">签单率66%</Col>
-            <Col span="6">%</Col>
-
-          </Row>
-          <Row class="order">
-            <Col span="6">30<br/>总接单数</Col>
-            <Col span="6">30<br/>总上门数</Col>
-            <Col span="6">30<br/>总签单数</Col>
-            <Col span="6">30<br/>总签单金额</Col>
-          </Row>
-          <h3>签单占比</h3>
-      </Tab>
+        <Tab title="本周">
+            <h3>数据统计</h3>
+            <Row class=mini>
+                <Col span="6">%</Col>
+                <Col span="6">上门率{{data.shangmen_rate * 100}}%</Col>
+                <Col span="6">签单率{{data.qiandan_rate * 100}}%</Col>
+                <Col span="6">%</Col>
+            </Row>
+            <Row class="order">
+                <Col span="6">{{data.all_num}}<br/>总接单数</Col>
+                <Col span="6">{{data.shangmen_num}}<br/>总上门数</Col>
+                <Col span="6">{{data.qiandan_num}}<br/>总签单数</Col>
+                <Col span="6">{{data.qiandan_fee}}<br/>总签单金额</Col>
+            </Row>
+            <h3>签单占比</h3>
+        </Tab>
       <Tab title="本月">
          <h3>数据统计</h3>
 
           <Row class=mini>
             <Col span="6">%</Col>
-            <Col span="6">上门率66%</Col>
-            <Col span="6">签单率66%</Col>
+            <Col span="6">上门率{{data.shangmen_rate * 100}}%</Col>
+            <Col span="6">签单率{{data.qiandan_rate * 100}}%</Col>
             <Col span="6">%</Col>
 
           </Row>
           <Row class="order">
-            <Col span="6">30<br/>总接单数</Col>
-            <Col span="6">30<br/>总上门数</Col>
-            <Col span="6">30<br/>总签单数</Col>
-            <Col span="6">30<br/>总签单金额</Col>
+            <Col span="6">{{data.all_num}}<br/>总接单数</Col>
+            <Col span="6">{{data.shangmen_num}}<br/>总上门数</Col>
+            <Col span="6">{{data.qiandan_num}}<br/>总签单数</Col>
+            <Col span="6">{{data.qiandan_fee}}<br/>总签单金额</Col>
           </Row>
           <h3>签单占比</h3>
       </Tab>
@@ -122,6 +119,8 @@
 
 <script>
 import { Cell, CellGroup, Popup, Row, Col, Picker, Toast , Tab,Tabs,} from 'vant';
+import F2 from '@antv/f2/lib/index-all';
+import PieLabel from '@antv/f2/lib/plugin/pie-label'; 
 import footerNav from '../../components/footerNav'
 import { datastatistical } from '@/server';
 
@@ -141,96 +140,144 @@ export default {
     },
     data () {
         return {
-            list:{},
-            active: 0
+            data: {},
+            active: 0,
+            list_all: []
         }
     },
     created() {
-      this.datastatistical()
+      this.datastatistical();
+    },
+    mounted() {
+        setTimeout(
+            () => {
+                this.createChart();
+            }, 4000
+        )
     },
     methods: {
        datastatistical(type) {
-          let params = {
-              'type': this.type,
-          }
-          datastatistical(params).then(
-            res => {
-              console.log(res)
-              // this.list = res.statistics;
-              }
-          )
-      },
+            let params = {
+                'type': this.type,
+            }
+            datastatistical(params).then(
+                res => {
+                    this.data = res.statistics;
+                    this.list_all = res.statistics.all_num_groupby_res;
+                    this.list_all.map(item => {
+                        item.const = 'const';
+                        item.num = +item.num;
+                        item.sSalesName = item.sSalesName || '-';
+                    })
+                }
+            )
+        },
+        createChart() {
+            let chart = new F2.Chart({
+                id: 'visit',
+                plugins: PieLabel
+            });
+            chart.source(this.list_all);
+            chart.coord('polar', {
+                transposed: true,
+                radius: 0.75
+            });
+            chart.legend(false);
+            chart.axis(false);
+            chart.tooltip(false);
+            // 添加饼图文本
+            chart.pieLabel({
+                sidePadding: 20,
+                label1: function label1(data, color) {
+                    return {
+                        text: data.sSalesName,
+                        fill: color
+                    };
+                },
+                label2: function label2(data) {
+                    return {
+                        text: data.num,
+                        fill: '#808080',
+                        fontWeight: 'bold'
+                    };
+                }
+            });
+            chart.interval().position('const*num').color('sSalesName', ['#1890FF', '#13C2C2', '#2FC25B', '#FACC14']).adjust('stack');
+            chart.render();
+        }
     },
 }
 </script>
 
 <style lang="scss"> 
-  .bossHome_container {
-    .mini{
-      text-align: center;
-      padding: 10px 0;
-      font-size: 10px;
-      color: #8797B6;
-      .van-col:first-child,
-      .van-col:last-child{
-        color:#fff;
-      }
-    }
-    .order{
-      text-align: center;
-      line-height: 1.5;
-      font-size: 12px;
-      color: #575757;
-      padding-bottom:10px;
-      border-bottom:1px solid #EBEBEB;
-    }
-  
-    a:link,a:visited,a:hover,a:active { 
-       font-size: 14px;
-      color: #575757;
-      }
-    h3{
-      font-size: 14px;
-      color: #262626;
-      font-family: PingFangSC-Medium;
-      padding:10px;text-align:center;
-    }
-    .homepage{
-      padding: 15px;display: flex;
-      flex-wrap: wrap ;
-      a{color:#575757;}
-    }
-    a{
-      width:25%;
-      text-align: center; 
-      margin-bottom:10%;
-    }
-    .alink{
-      font-size: 14px;
-    }
-    .all_user{
-      width:60%;
-      // height:73px;
-      border-radius:50%;
-      display:block;
-      margin: 0 auto 10px auto;
-    }
-    .rank{
-      padding:10px 0;
-      display:flex; 
-      justify-content: space-between;
-      border-top: 1px solid #EBEBEB;
-      border-bottom: 1px solid #EBEBEB;
-      a{
-        line-height:100%;height:90px;
-        margin:0;
-        border-right: 1px solid #EBEBEB;
-        width:33%;
-        // text-align:center;
-        img{width:auto;display:block; height:50%;margin:10px auto;max-width:70%;}
+    .bossHome_container {
+        .piechart {
+            width: 100%;
+            height: auto;
+        }
+        .mini{
+            text-align: center;
+            padding: 10px 0;
+            font-size: 10px;
+            color: #8797B6;
+            .van-col:first-child,
+            .van-col:last-child{
+                color:#fff;
+            }
+        }
+        .order{
+            text-align: center;
+            line-height: 1.5;
+            font-size: 12px;
+            color: #575757;
+            padding-bottom:10px;
+            border-bottom:1px solid #EBEBEB;
+        }
+        a:link,a:visited,a:hover,a:active { 
+            font-size: 14px;
+            color: #575757;
+        }
+        h3{
+            font-size: 14px;
+            color: #262626;
+            font-family: PingFangSC-Medium;
+            padding:10px;text-align:center;
+        }
+        .homepage{
+            padding: 15px;display: flex;
+            flex-wrap: wrap ;
+            a{color:#575757;}
+        }
+        a{
+            width:25%;
+            text-align: center; 
+            margin-bottom:10%;
+        }
+        .alink{
+            font-size: 14px;
+        }
+        .all_user{
+            width:60%;
+            // height:73px;
+            border-radius:50%;
+            display:block;
+            margin: 0 auto 10px auto;
+        }
+        .rank{
+            padding:10px 0;
+            display:flex; 
+            justify-content: space-between;
+            border-top: 1px solid #EBEBEB;
+            border-bottom: 1px solid #EBEBEB;
+            a{
+                line-height:100%;height:90px;
+                margin:0;
+                border-right: 1px solid #EBEBEB;
+                width:33%;
+                // text-align:center;
+                img{width:auto;display:block; height:50%;margin:10px auto;max-width:70%;}
 
-      }
-
+            }
+        }
     }
-  }
 </style>
